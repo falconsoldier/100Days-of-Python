8f57f715c53113b91a6d23caa5d5053bffcc3b9d
Andrew Gove <andrewgove56@gmail.com>
2023-12-03 10:21:52 -0500

    Finished calculator

--- Day10.py
+++ Day10.py
@@ -1,4 +1,32 @@
+def add(n, m):
+    return n+m
+def subtract(n, m):
+    return n-m
+def multiply(n, m):
+    return n*m
+def divide(n, m):
+    return n/m
+
+
+
 def calc():
-    return 1
+    ops = {'+': add, '-': subtract, '*': multiply, '/': divide}
+    print("Hello There, this is a lazy calculator app")
+    print("It will take two numbers and do something with them")
+    num = float(input("What's the first number? "))
+
+    for x in ops:
+        print(x)
+    opsin = input("Pick an operation: ")
+    num2 = float(input("What is the second number? "))
+    calcf = ops[opsin]
+    ans = calcf(num, num2)
+    print(f"{num} {opsin} {num2} = {ans}")
+
+
+
+
+
+
 
 calc()
