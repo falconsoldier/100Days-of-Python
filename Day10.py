0bbb29c3c223861b6c43a1388696f49449171544
Andrew Gove <andrewgove56@gmail.com>
2023-12-03 12:55:44 -0500

    Catching more exceptions

--- Day10.py
+++ Day10.py
@@ -10,21 +10,39 @@ def divide(n, m):
     except ZeroDivisionError:
         m = float(input("You cannot divide by zero, please select a different number: "))
         return divide(n,m)
-    return ans
+    return [ans, n, m]
 
 def calc():
+    n1b = True
+    n2b = True
+    nob = True
+
     ops = {'+': add, '-': subtract, '*': multiply, '/': divide}
     print("Hello There, this is a lazy calculator app")
     print("It will take two numbers and do something with them")
-    num = float(input("What's the first number? "))
-
+    while n1b:
+        try:
+            num = float(input("What's the first number? "))
+            n1b = False
+        except ValueError:
+            print("You need to enter a number")
     for x in ops:
         print(x)
-    opsin = input("Pick an operation: ")
-    num2 = float(input("What is the second number? "))
+    while nob:
+        opsin = input("Pick an operation: ")
+        if opsin not in ops:
+            print("Please pick a valid operation")
+        else:
+            nob = False
+    while n2b:
+        try:
+            num2 = float(input("What is the second number? "))
+            n2b = False
+        except ValueError:
+            print("Please select a number")
     calcf = ops[opsin]
     ans = calcf(num, num2)
-    print(f"{num} {opsin} {num2} = {ans}")
+    print(f"{ans[1]} {opsin} {ans[2]} = {ans[0]}")
 
 
 
