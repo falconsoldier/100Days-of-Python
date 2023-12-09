a7ef82f85fead35b43dc8e67a43502a510fd2a9b
Andrew Gove <andrewgove56@gmail.com>
2023-12-08 21:12:08 -0500

    small change and bug fixes

--- Day11.py
+++ Day11.py
@@ -1,17 +1,27 @@
-class Invaliddeck(Exception):
-    "Raised when the input value not 0-9"
-    pass
+def playround(rdeck):
+    return 1
 
 
 def main():
     sdeck = {"One": 1, "Two": 2, "Three":3, "Four": 4, "Five":5, "Six":6, "Seven":7,"Eight":8,"Nine":9,"Ten":10, "Jack": 10, "Queen":10,"King":10,"Ace":11}
     rdeck = []
     d = setup()
-    print("Lets do one deck to start")
+    print("The number of decks will be ",d)
     for x in range(d):
         rdeck += list(sdeck.keys())
     print(rdeck)
-
+    while True:
+        playround(rdeck)
+        #ans = input("blblbl")
+        while True:
+            print("Do you wnat to keep playing?")
+            ans = input("Y/N: ")
+            if ans.lower() == 'y' or ans.lower() == 'n':
+                break
+            else:
+                print("Please answer the questions")
+        if ans.lower() == 'n':
+            break
 
 
 def setup():
@@ -19,10 +29,11 @@ def setup():
     print("How many decks would you like to play with?")
     while True:
         try:
-            decks = int(input("Enter a number 0-9: "))
+            decks = int(input("Enter a number 1-9: "))
             if decks > 9 or decks < 1:
-                raise Invaliddeck
-            break
+                print("Please pick a number 1-9")
+            else:
+                break
         except ValueError:
             print("Please enter a number")
     return decks
