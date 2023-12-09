22a363037acb2a62add2c331d3622a7dac1d48f8
Andrew Gove <andrewgove56@gmail.com>
2023-12-08 23:22:36 -0500

    quick fixes

--- Day11.py
+++ Day11.py
@@ -72,7 +72,7 @@ def main():
     print("The number of decks will be ",d)
     for x in range(d):
         rdeck += list(sdeck.keys())
-    print(rdeck)
+    #Is this readable
     while True:
         playround(rdeck, sdeck)
         while True:
@@ -102,8 +102,4 @@ def setup():
 
 
 
-sdeck = {"Two": 2, "Three":3, "Four": 4, "Five":5, "Six":6, "Seven":7,"Eight":8,"Nine":9,"Ten":10, "Jack": 10, "Queen":10,"King":10,"Ace":11}
-rdeck = []
-for x in range(4):
-    rdeck += list(sdeck.keys())
-playround(rdeck, sdeck)
+main()
