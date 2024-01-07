c81db10d5e700254068ec6ea6c163560f7ec6335
Andrew Gove <andrewgove56@gmail.com>
2024-01-07 12:19:03 -0500

    Finished Quiz Game Day17

--- Day17/QuizBrain.py
+++ Day17/QuizBrain.py
@@ -4,4 +4,10 @@ class QuizBrain:
         self.score = 0
         self.qlist = quizlist
     
+    def still_has_question(self):
+        return self.qnum < len(self.qlist)-1
     
+    def question(self, jc):
+        print(jc["text"])
+        ans = input("True or False: ")
+        return ans
